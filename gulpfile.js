d2183d8d413aa6c774b25c7c9d101282b07484d8
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-10-30 18:39:43 -0500

    dev env setup

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+//required
+const gulp = require('gulp'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cleanCSS = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  htmlmin = require('gulp-htmlmin'),
+  sass = require('gulp-sass');
+
+//tasks
+gulp.task('styles', () => {
+  console.log('styles ran');
+  gulp.src('public/styles/sass/main.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      compatibility: '*'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('minify', function() {
+  return gulp.src('public/views/src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('public/views'));
+});
+
+//watch tasks
+gulp.task('watch:styles', () => {
+  gulp.watch('public/styles/sass/*.sass', ['styles']);
+});
+
+//default tasks
+gulp.task('default', ['watch:styles']);
