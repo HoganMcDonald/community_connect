ac2027e8d42439daed836056fe55ed14a17af3c1
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-10-31 14:28:15 -0500

    register form styled

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -3,8 +3,12 @@
 
 $(document).ready(()=> {
 
-  $('.search-input').focus(()=> {
+  $('.search-input').focusin(()=> {
     $('.search-icon').css('left', '250px');
   }); // end focus event for search input
+
+  $('.search-input').focusout(()=> {
+    $('.search-icon').css('left', '30px');
+  }); // end leave focus event for search input
 
 });
